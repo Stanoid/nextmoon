ebb1d9728dffd0682ee5825221a7207521b2e385
Stanoid <mujahidwadmohammad@gmail.com>
2024-07-30 18:35:31 +0300

    nextcommit

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,10 @@ const nextConfig = {
             protocol: 'http',
             hostname: '**',
           },
+          {
+            protocol: 'https',
+            hostname: '**',
+          },
         ],
       },
 
