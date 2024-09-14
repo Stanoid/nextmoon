40c9e1b4e38f4781d8387dd2220dec04614a9287
Stanoid <mujahidwadmohammad@gmail.com>
2024-09-14 16:44:38 +0300

    added login waiting and toast

--- src/app/comps/navbar.js
+++ src/app/comps/navbar.js
@@ -224,7 +224,7 @@ text-xs font-medium         text-white mx-1 rounded-md " size="sm"    aria-label
 
 
 
-{userData&&userData.error?<motion.div 
+{userData&&!userData.error?<motion.div 
   whileHover={{ scale: 1.03 }}
   whileTap={{ scale: 0.9 }}
   transition={{ type: "spring", stiffness: 400, damping: 17 }}
