503427b320765d18d93b7d930dc5cf2e4072535b
stanoid <mujahidwadmohammad@gmail.com>
2024-12-19 08:23:12 +0300

    localc

--- src/app/local.js
+++ src/app/local.js
@@ -26,8 +26,8 @@ export const DEF_IMG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAANcAAACUCA
    //     export const IMG_URL = process.env.NEXT_PUBLIC_IMG_URL || "http://192.168.96.110:1337"
    //  export const API_URL = process.env.NEXT_PUBLIC_API_URL ||    "http://192.168.96.110:1337/api/"
 
-   export const IMG_URL = process.env.NEXT_PUBLIC_IMG_URL || "http://localhost:1337"
-   export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/api/"
+   // export const IMG_URL = process.env.NEXT_PUBLIC_IMG_URL || "http://localhost:1337"
+   // export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/api/"
 
 //eeeeeeeeeeeeeeeeeeeeeeeee
 
